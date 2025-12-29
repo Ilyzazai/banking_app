33fe0eef8700ecca69541f9a811b2aad6630b4d2
Ilyas <ilyas.zazai97@gmail.com>
2025-12-28 20:25:09 -0500

    table has created in mysql database

--- src/main/java/banking_app/repository/AccountRepository.java
+++ src/main/java/banking_app/repository/AccountRepository.java
@@ -1,4 +1,7 @@
 package banking_app.repository;
 
-public interface AccountRepository {
+import banking_app.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account,Long> {
 }
